bdb912d8425ea335f66da2b91c03c0643c557fa6
David <davidtr@utexas.edu>
2023-03-03 15:04:37 -0600

    frontend

--- sport-app/src/App.js
+++ sport-app/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-function App() {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [favoriteSports, setFavoriteSports] = useState([]);
+  const [sport, setSport] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      setLoggedIn(true);
+      // Retrieve the user's favorite sports from the database
+      const userRef = firebase.firestore().collection("users").doc(firebase.auth().currentUser.uid);
+      const doc = await userRef.get();
+      if (doc.exists) {
+        setFavoriteSports(doc.data().favoriteSports);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleLogout = () => {
+    firebase.auth().signOut();
+    setLoggedIn(false);
+  };
+
+  const handleAddSport = async () => {
+    try {
+      // Update the user's favorite sports in the database
+      const userRef = firebase.firestore().collection("users").doc(firebase.auth().currentUser.uid);
+      await userRef.update({
+        favoriteSports: firebase.firestore.FieldValue.arrayUnion(sport)
+      });
+      setFavoriteSports([...favoriteSports, sport]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {{textAlign: "center"}}>
+      {loggedIn ? (
+        <div>
+          <h1>Welcome, {email}!</h1>
+          <h3>Add a favorite sport:</h3>
+          <input type="text" placeholder="Sport" value={sport} onChange={(e) => setSport(e.target.value)}/>
+          <button onClick={handleAddSport}>Add</button>
+          <h2>Your favorite sports:</h2>
+          <ul>
+            {favoriteSports.map((sport) => (
+              <li key={sport}>{sport}</li>
+            ))}
+          </ul>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+       ) : (
+        <div>
+          <h1>Login</h1>
+          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default Login;
